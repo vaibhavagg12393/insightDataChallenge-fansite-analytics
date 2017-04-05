5f5e0405eecb96439a9285898cd1e09952ba33fe
Vaibhav Aggarwal <vaibhavagg12393@gmail.com>
2017-04-05 12:10:02 -0400

    implement feature 3

--- src/process_log.py
+++ src/process_log.py
@@ -1,28 +1,25 @@
-// your Python code to implement the features could be placed here
-// note that you may use any language, there is no preference towards Python
-import time
+# your Python code to implement the features could be placed here
+# note that you may use any language, there is no preference towards Python
 import os
 import re
 import operator
+import time
+import datetime
 
-fname = os.path.realpath("/Users/Vaibhav/Documents/fansite-analytics-challenge/log_input/log_input.txt")
-feature1_out = os.path.realpath("/Users/Vaibhav/Documents/fansite-analytics-challenge/log_output/hosts.txt")
-feature2_out = os.path.realpath("/Users/Vaibhav/Documents/fansite-analytics-challenge/log_output/resources.txt")
-feature3_out = os.path.realpath("/Users/Vaibhav/Documents/fansite-analytics-challenge/log_output/hours.txt")
-
-
-# Create a graph to store sender with their receivers and vice-versa
-# used a Set because Sets are significantly faster when it comes to determining if an object is
-# present in the set (as in x in s)
+fname = os.path.realpath("/Users/Vaibhav/Documents/insightDataChallenge-fansite-analytics/log_input/log_input.txt")
+feature1_out = os.path.realpath("/Users/Vaibhav/Documents/insightDataChallenge-fansite-analytics/log_output/hosts.txt")
+feature2_out = os.path.realpath("/Users/Vaibhav/Documents/insightDataChallenge-fansite-analytics/log_output/resources.txt")
+feature3_out = os.path.realpath("/Users/Vaibhav/Documents/insightDataChallenge-fansite-analytics/log_output/hours.txt")
+feature4_out = os.path.realpath("/Users/Vaibhav/Documents/insightDataChallenge-fansite-analytics/log_output/blocked.txt")
 
 def past_graph():
-    open(feature3_out,'w')
     with open(fname) as fp:
         for line in fp:
             try:
                 components = line.strip().split(" ")
                 host = re.search(r'([\w.-]+[.]*[\w.-])', line)
                 resource = re.search(r'\"(.+?)\"', line)
+                dateTimeStamp = re.search(r'\[(.+?)\]', line)
                 http = components[-2].strip()
                 bytes = components[-1].strip()
                 if host and http=='200':
@@ -35,6 +32,14 @@ def past_graph():
                         if mResource != "/" and isinstance(int(bytes), int):
                             graph2.setdefault(mResource, 0)
                             graph2[mResource] += int(bytes)
+                    except:
+                        continue
+                if dateTimeStamp:
+                    try:
+                        mDateTimeStamp = dateTimeStamp.group()
+                        mTime = str(mDateTimeStamp).split(" ")[-2].strip()
+                        mTimestamp = time.mktime(datetime.datetime.strptime(mTime, "[%d/%b/%Y:%H:%M:%S").timetuple())
+                        timeArray.append(mTimestamp)
                     except:
                         continue
             except ValueError:
@@ -51,34 +56,33 @@ def feature(graph,filename):
     except:
         return 0
 
+def getDateTime(timestamp):
+    local_time = time.localtime(timestamp)
+    dateTimeFormat = time.strftime("%d/%b/%Y:%H:%M:%S -0400", local_time)
+    return dateTimeFormat
 
-def getTime(timeinput):
-    timeinput = timeinput.split()[:-1]
-    timestamp = time.mktime(time.strptime(timeinput[0], '%d/%b/%Y:%H:%M:%S'))
-    return timestamp
-
-def slidingWindow(dict_time, time_arr, window):
+def feature3(graph3, time_arr, block):
+    time_arr = sorted(time_arr)
     i = 0
     while i < len(time_arr):
-        val1 = getTime(time_arr[i][0])
-        dict_time[time_arr[i][0]] = 1
+        previousKey = time_arr[i]
+        key = getDateTime(previousKey)
+        graph3[key] = 1
         j = i + 1
         while j < len(time_arr):
-            val2 = getTime(time_arr[j][0])
-            if val2 - val1 > window:
+            nextKey = time_arr[j]
+            if nextKey - previousKey > block:
                 break
             else:
-                dict_time[time_arr[i][0]] += 1
-            print('.')
+                graph3[key] += 1
             j += 1
         i += 1
-    print('Done sliding window')
 
 if __name__ == '__main__':
     graph = {}
-    dict_time={}
-    time_arr = []
     graph2={}
+    graph3 = {}
+    timeArray = []
     start = time.time()
     past_graph()
     end = time.time()
@@ -91,9 +95,13 @@ if __name__ == '__main__':
     feature(graph2,feature2_out)
     end = time.time()
     print "Feature 2 time: %.2f seconds" % (end - start)
-    slidingWindow(dict_time, time_arr, 60)
-    print len(dict_time)
-    feature(dict_time,feature3_out)
+    start = time.time()
+    print len(timeArray),len(graph3)
+    feature3(graph3, timeArray, 3600)
+    end = time.time()
+    print "Feature 3 time: %.2f seconds" % (end - start)
+    print len(graph3)
+    feature(graph3,feature3_out)
 
 '''
 Feature 1 without http 200
@@ -134,5 +142,23 @@ www-d3.proxy.aol.com,5675
 /shuttle/countdown/video/livevideo.gif,1128333928
 /shuttle/missions/sts-71/movies/sts-71-mir-dock.mpg,1095681435
 
+13/Jul/1995:08:59:33 -0400,34968
+13/Jul/1995:08:59:40 -0400,34960
+13/Jul/1995:08:59:39 -0400,34960
+13/Jul/1995:08:59:38 -0400,34955
+13/Jul/1995:08:59:42 -0400,34952
+13/Jul/1995:08:59:35 -0400,34951
+13/Jul/1995:08:59:34 -0400,34950
+13/Jul/1995:08:59:31 -0400,34949
+13/Jul/1995:08:59:41 -0400,34947
+13/Jul/1995:08:58:54 -0400,34944
+
+Graph contruction: 147.62 seconds
+Feature 1 time: 1.16 seconds
+Feature 2 time: 0.02 seconds
+4380920 0
+Done sliding window
+Feature 3 time: 10440.36 seconds
+1104525
 
 '''
